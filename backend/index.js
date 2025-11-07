cd643a9921c65c0d8b82830074b6564099f896fc
Sachyam28 <sachyamshakya@gmail.com>
2025-11-07 12:35:04 +0530

    index.js chnge mongodb conn method

--- backend/index.js
+++ backend/index.js
@@ -32,10 +32,31 @@ app.get("/", (req, res) => {
   res.send("Webstories backend is working ✅");
 });
 
-// ✅ Connect MongoDB once
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log("✅ MongoDB connected"))
-  .catch(err => console.error("❌ Mongo Error:", err));
+let isConnected = false;
+
+// ✅ Connect to MongoDB
+async function connectToDB() {
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
+    isConnected = true;
+    console.log("✅ MongoDB connected");
+  } catch (err) {
+    console.error("❌ Mongo Error:", err);
+  }
+}
+
+//middleware to check DB connection
+app.use(async (req, res, next) => {
+  if (!isConnected) {
+    connectToDB();
+  }
+  next();
+});
+
+
 
 // ✅ DO NOT app.listen() on Vercel
 // app.listen(PORT) ❌ REMOVE
